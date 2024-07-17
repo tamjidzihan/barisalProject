0eccc1cbf7e2e7d85f1dd9b9dfe6e3a66cb03d05
tamjidzihan <tamjidzihan@gmail.com>
2024-07-18 00:06:18 +0600

    Home 2024-07-18

--- client/src/components/Home/Banner.tsx
+++ client/src/components/Home/Banner.tsx
@@ -4,6 +4,7 @@ const Banner = () => {
         <div>
             <div className=" bg-gradient-to-b from-[#8a173f]  to-[#03ab9c] h-80 md:h-[14rem] lg:h-[18rem] rounded-b-full flex flex-col justify-center items-center  ">
                 <h2 className=' text-white font-bold text-lg md:text-3xl lg:text-4xl mb-10'>E-Service Of Barishal</h2>
+                <h2 className=' text-white font-bold text-lg md:text-3xl lg:text-4xl mb-10'>Injamam's Barisal Project</h2>
                 <p className='text-white text-center px-10 md:text-2xl lg:text-3xl'>
                     বরিশাল জেলার সকল সার্ভিস এবং লোকেশন <br /> সম্পর্কে জানতে আমাদের সাথেই থাকুন।
                 </p>
