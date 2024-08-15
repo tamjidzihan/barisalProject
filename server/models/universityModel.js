51253f57acd6e495d7bded45c2bcdc44f8ce4cb6
tamjidzihan <tamjidzihan@gmail.com>
2024-08-15 16:13:24 +0600

    added website field to uv

--- server/models/universityModel.js
+++ server/models/universityModel.js
@@ -1,60 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const universityDetailSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        required: false
-    },
-    name: {
-        type: String,
-        required: false
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    address: {
-        type: String,
-        required: false
-    },
-    founded: {
-        type: Number,
-        required: false
-    },
-    campus: {
-        type: String,
-        required: false
-    },
-    students: {
-        type: Number,
-        required: false
-    }
+  type: {
+    type: String,
+    required: false,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  website: {
+    type: String,
+    required: false,
+  },
+  founded: {
+    type: Number,
+    required: false,
+  },
+  campus: {
+    type: String,
+    required: false,
+  },
+  students: {
+    type: Number,
+    required: false,
+  },
 });
 
 const universitySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    slug: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    result: [universityDetailSchema]
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  result: [universityDetailSchema],
 });
 
-const modelName = 'University';
+const modelName = "University";
 
 module.exports = mongoose.model(modelName, universitySchema);
