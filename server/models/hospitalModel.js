b0f77148756b0614052433d2872b07927715f80c
imjamam <tamjidzihan@gmail.com>
2024-08-16 09:27:28 +0600

    change some features for hospital in office 16aug

--- server/models/hospitalModel.js
+++ server/models/hospitalModel.js
@@ -1,59 +1,64 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const hospitalDetailSchema = new mongoose.Schema(
-    {
-        type: {
-            type: String,
-            required: false
-        },
-        name: {
-            type: String,
-            required: false
-        },
-        image: {
-            type: String,
-            required: false
-        },
-        phone: {
-            type: String,
-            required: false
-        },
-        address: {
-            type: String,
-            required: false
-        },
-        founded: {
-            type: Number,
-            required: false
-        }
-    }
-)
-
-const hospitalSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    slug: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    result: [hospitalDetailSchema]
+const hospitalDetailSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: false,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  website: {
+    type: String,
+    required: false,
+  },
+  founded: {
+    type: Number,
+    required: false,
+  },
 });
 
+const hospitalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  result: [hospitalDetailSchema],
+});
 
-const modelName = 'Hospital'
+const modelName = "Hospital";
 
-module.exports = mongoose.model(modelName, hospitalSchema)
+module.exports = mongoose.model(modelName, hospitalSchema);
