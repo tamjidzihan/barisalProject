e98c510884366d55b39338b74434eebff01104ae
tamjidzihan <tamjidzihan@gmail.com>
2024-08-29 16:15:00 +0600

    office 2024-08-29

--- server/seeds/realEstateSeed.js
+++ server/seeds/realEstateSeed.js
@@ -3,12 +3,10 @@ const { MONGO_URI } = require("../constants");
 
 const uri = MONGO_URI;
 
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-//Define detail schema
 const realEstateDetailSchema = new mongoose.Schema({
   type: {
     type: String,
@@ -42,13 +40,12 @@ const realEstateDetailSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
-
   Location: {
     type: String,
     required: false,
   },
 });
-//Define service schema
+
 const realEstateServiceSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -72,28 +69,59 @@ const realEstateServiceSchema = new mongoose.Schema({
   },
   result: [realEstateDetailSchema],
 });
-// Create model based on the schema
-const realEstateService = mongoose.model("RealEstate", realEstateServiceSchema);
+
+const RealEstate = mongoose.model("RealEstate", realEstateServiceSchema);
 
 // Data to be inserted
-const realEstateServiceData = {
-  name: "Real Estate Service in Barisal",
-  type: "Others",
+const realEstateData = {
+  name: "Real Estate Services in Barisal",
+  type: "Real Estate",
+  slug: "realestateservices",
   image: "", // Add URL for image if available
-  slug: "realEstate",
-  description: "List of RealEstate Service of Barisal",
+  description: "List of Real Estate Services in Barisal",
   result: [
     {
-      type: "RealEstate",
+      name: "CIVIL DEVELOPMENT",
+      image: "https://i.ibb.co/g6J7yJk/civil-development.webp",
+      address: "House-1456, Rajumiar Goli, Battala, Barisal, Bangladesh, 8200",
+      email: "info@civildevelopment.org",
+      website: "https://civildevelopment.org/",
+      phone: "+880 1925-396631",
     },
-  ],
+    {
+      name: "Rahmania Properties",
+      image: "https://i.ibb.co/jbVnz0Y/Rahmania-properties.webp",
+      address: "ভূঁইয়া ম্যানশন-১৪৫৬, রাজু মিয়াঁর গলি, Major M A Jalil Rd, বরিশাল। 8200",
+      email: "info@rahmaniaproperties.com",
+      website: "https://www.rahmaniaproperties.com/",
+      phone: "01635-220370",
+    },
+    {
+      name: "Hillview Housing Ltd",
+      image: "https://i.ibb.co/rtyNyz4/Hillview-Housing-Ltd.webp",
+      address: "Islam Tower, Holding 1039 College Row, Barishal 8200",
+      email: "info@hillviewbd.com",
+      website: "https://hillviewbd.com/",
+      phone: "+880 1719449091",
+    },
+    {
+      name: "ENSURE LANDMARK LTD",
+      image: "https://i.ibb.co/rtyNyz4/Hillview-Housing-Ltd.webp",
+      address: "P938+V47 Kobi Jibonando Dash Lane, Bogra Rd, Barishal",
+      email: "info@ensuregroup.com.bd",
+      website: "https://www.ensuregroup.com.bd/",
+      phone: "+88-09613-868686",
+    },
+    {
+      name: "ADC Holdings Limited",
+      address: "2nd Floor, Hasina Mahol, H#566, Bangla Bazar Rd, Barishal 8200",
+      email: "info@adc-bd.com",
+      website: "www.adcgroup-bd.com",
+      phone: "01727-012346",
+    }
+  ]
 };
 
-// Inserting data into MongoDB
-realEstateService
-  .create(realEstateServiceData)
-  .then(() => {
-    console.log("realEstate service data inserted");
-    mongoose.connection.close(); // Close connection after insertion
-  })
-  .catch((err) => console.error(err));
+RealEstate.create(realEstateData)
+  .then(() => console.log('Real estate service data inserted'))
+  .catch(err => console.log(err));
